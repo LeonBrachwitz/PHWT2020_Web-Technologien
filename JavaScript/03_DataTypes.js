52d8daab83733856e5336f9f7c642d04ec6465fb
Leon Brachwitz <mail@leon-brachwitz.de>
2021-05-07 11:33:52 +0200

    Einzelne HTML-Dateien und DataTypes

---
+++ JavaScript/03_DataTypes.js
@@ -0,0 +1,39 @@
+console.log("Skript 3:");
+
+var logischeVariable = true;
+var Zeichenkette = "Text";
+var Zahl = 10;
+
+// Weiterhin: Objekte
+// Arrays und Funktionen sind spezielle Objekte
+
+// Zuweisung
+
+logischeVariable = false;
+Zahl = 20;
+
+// Rechenoperatroen
+
+Zahl = Zahl - 10;
+Zahl += 5;
+Zahl++;
+
+// Vergleiche
+
+if (Zahl == 16) {
+    console.log("Stimmt.")
+};
+
+if (1 == true) {
+    console.log("1 ist gleich true.")
+}
+else {
+    console.log("1 ist doch nicht gleich true...")
+};
+
+// Typen und Ausdrücke - Es kann verwirrend sein ...
+
+logischeVariablen = true;
+var testZahl;
+testZahl = Zahl + logischeVariable;
+console.log(testZahl);
